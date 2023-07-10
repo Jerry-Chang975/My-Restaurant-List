f4e3a34bc1203a67b1128b4c42f38483fae5a9d5
Jerry-Chang975 <jerry975607@gmail.com>
2023-07-11 00:13:28 +0800

    feat: add lising & detail of restaurants

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const restaurants = require('./public/jsons/restaurant.json');
+const restaurants = require('./public/jsons/restaurant.json').results;
+const { engine } = require('express-handlebars');
 
+app.engine('.hbs', engine({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+app.set('views', './views');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
@@ -10,12 +14,15 @@ app.get('/', (req, res) => {
 });
 
 app.get('/restaurants', (req, res) => {
-  res.send('restaurants');
+  res.render('index', { restaurants });
 });
 
-app.get('/restaurant/:id', (req, res) => {
+app.get('/restaurants/:id', (req, res) => {
   const id = req.params.id;
-  res.send(`restaurant: ${id}`);
+  const matchedRestaurant = restaurants.find(
+    (restaurant) => restaurant.id.toString() === id
+  );
+  res.render('detail', { restaurant: matchedRestaurant });
 });
 
 app.listen(port, () => {
